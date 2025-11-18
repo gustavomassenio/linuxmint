521cbd9902539827a63e12f16cbbcbc5fac76db2
Gustavo Massenio <gustavomassenio@gmail.com>
2025-11-17 19:50:44 -0500

    atualização de dados via linux

---
+++ comandos/comandos.sh
@@ -0,0 +1,46 @@
+   1  sudo apt update
+    2  sudo apt upgrade 
+    3  sudo apt upgrade --fix-missing
+    4  exit
+    5  power off
+    6  poweroff
+    7  sudo apt install curl
+    8  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    9  sudo apt install curl
+   10  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+   11  sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+   12  sudo apt update
+   13  sudo apt install brave-browser
+   14  exit
+   15  sudo apt update && sudo apt upgrade
+   16  ~sudo apt install torbrowser-launcher
+   17  ``` :contentReference[oaicite:3]{index=3}  
+   18  sudo apt install
+   19  sudo apt update && sudo apt upgrade
+   20  sudo apt install torbrowser-launcher
+   21  ``` :contentReference[oaicite:3]{index=3}  
+   22  ~
+   23  sudo apt install torbrowser-launcher
+   24  ``` :contentReference[oaicite:3]{index=3}  
+   25  tor
+   26  sudo apt install torbrowser-launcher
+   27  exit
+   28  sudo apto update
+   29  sud apto update
+   30  sudo apt update
+   31  sudo apt install git vim notepadqq 
+   32  vim
+   33  sudo init 0
+   34  sudo apt update
+   35  sudo
+   36  sudo -i
+   37  sudo apt update 
+   38  sudo apt upgrade
+   39  sudo apt dist-upgrade 
+   40  sudo apt full-upgrade 
+   41  sudo apt autoremove 
+   42  sudo apt autoclean 
+   43  sudo apt clean 
+   44  sudo apt update
+   45  sudo apt install git vim pip python3
+   46  history
